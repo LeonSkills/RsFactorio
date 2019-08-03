1a3cd5d6f85f1593db28dd4693780f059ff68c6c
LeonSkills <leonskillsho@gmail.com>
2019-08-04 01:26:08 +0200

    Added all herblore potions and their secondaries as items

---
+++ mods/Herblore/mod/prototypes/group.lua
@@ -0,0 +1,28 @@
+
+data:extend({
+  {
+    type = "item-group",
+    name = "rs-herblore",
+    order = "rs-051-herblore",
+    icon = "__RsHerblore__/graphics/item-group/herblore.png",
+    icon_size = 128
+  },
+  {
+    type = "item-subgroup",
+    name = "rs-regular-potions",
+    order = "rs-herblore-020-regular-potions",
+    group = "rs-herblore"
+  },
+  {
+    type = "item-subgroup",
+    name = "rs-combination-potions",
+    order = "rs-herblore-021-regular-potions",
+    group = "rs-herblore"
+  },
+  {
+    type = "item-subgroup",
+    name = "rs-herblore-secondaries",
+    order = "rs-herblore-030-secondaries",
+    group = "rs-herblore"
+  }
+})
