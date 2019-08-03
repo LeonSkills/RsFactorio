3afb6a037157b72ccfe6b017d7b1dd2ca9752a04
LeonSkills <leonskillsho@gmail.com>
2019-08-03 22:47:33 +0200

    Pipe pictures of assemblers are now tinted based on the material, not based on the vanilla assembler

--- mods/AssemblingMachines/mod/prototypes/assembler-entities.lua
+++ mods/AssemblingMachines/mod/prototypes/assembler-entities.lua
@@ -15,6 +15,13 @@ local assembler_data = {
   [3] = {width = 214, height = 237, shift = {0, -0.0234375}, shadow_animation = copies[3].animation.layers[2]}
 }
 
+local pipe_data = {
+  east = {height = 76, width = 42, shift = {-0.78125, 0.03125}},
+  north = {height = 38, width = 71, shift = {0.0703125, 0.421875}},
+  south = {height = 61, width = 88, shift = {0, -0.9765625}},
+  west = {height = 73, width = 39, shift = {0.8046875, 0.0390625}}
+}
+
 for tier, item_data in pairs(assemblers) do
   local entity = table.deepcopy(copies[item_data.based_on])
   entity.name = get_vanilla_name( "rs-"..item_data.material .."-assembling-machine")
@@ -34,7 +41,7 @@ for tier, item_data in pairs(assemblers) do
   entity.crafting_speed = item_data.speed
   entity.energy_usage =   item_data.energy_consumption
   entity.minable.result = entity.name
-  entity.next_upgrade = item_data.next_upgrade or entity.next_upgrade
+  entity.next_upgrade = get_vanilla_name(item_data.next_upgrade or entity.next_upgrade)
   if item_data.energy_type == "burner" then
     entity.energy_source = {
       type = "burner",
@@ -69,6 +76,36 @@ for tier, item_data in pairs(assemblers) do
       assembler_data[item_data.based_on].shadow_animation
     }
   }
+  -- add pipe connections
+  if item_data.based_on >= 2 then
+    for _, dir in pairs({"north", "east", "south", "west"}) do
+      for i = 1, 2 do
+        entity.fluid_boxes[i].pipe_picture[dir] = {
+          layers = {
+            {
+              filename = "__RsAssemblers__/graphics/entity/assembler-blocks/pipe-"..dir..".png",
+              height = pipe_data[dir].height,
+              width = pipe_data[dir].width,
+              shift = pipe_data[dir].shift,
+              scale = 0.5,
+              priority = "extra-high",
+            },
+            {
+              filename = "__RsAssemblers__/graphics/entity/assembler-blocks/pipe-base-"..dir..".png",
+              height = pipe_data[dir].height,
+              width = pipe_data[dir].width,
+              shift = pipe_data[dir].shift,
+              scale = 0.5,
+              priority = "extra-high",
+              tint = colours[item_data.material].medium
+            },
+          }
+        }
+      end
+
+    end
+
+  end
   entity.localised_name = {"item-names.rs-"..item_data.material.."-assembling-machine"}
   data.raw["assembling-machine"][entity.name] = nil
   data:extend({entity})
