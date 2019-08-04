02e4cef183b83d564be3f075a44ae3f4e94eb13e
LeonSkills <leonskillsho@gmail.com>
2019-08-04 22:00:51 +0200

    Created potion recipes

---
+++ mods/Herblore/mod/data/combination_potion_data.lua
@@ -0,0 +1,172 @@
+return {
+  ["grand-strength-potion"] = {
+    potions = {{"rs-super-strength", 3}, {"rs-strength-potion", 3}},
+    level = 65,
+    price = 100000
+  },
+  ["grand-ranging-potion"] = {
+    potions = {{"rs-super-ranging-potion", 3}, {"rs-ranging-potion", 3}},
+    level = 76,
+    price = 100000
+  },
+  ["grand-magic-potion"] = {
+    potions = {{"rs-super-magic-potion", 3}, {"rs-magic-potion", 3}},
+    level = 75,
+    price = 100000
+  },
+  ["grand-attack-potion"] = {
+    potions = {{"rs-super-attack", 3}, {"rs-attack-potion", 3}},
+    level = 78,
+    price = 100000
+  },
+  ["grand-defence-potion"] = {
+    potions = {{"rs-super-defence", 3}, {"rs-defence-potion", 3}},
+    level = 70,
+    price = 100000
+  },
+  ["super-melee-potion"] = {
+    potions = {{"rs-super-defence", 3}, {"rs-super-strength", 3}, {"rs-super-attack", 3}},
+    level = 81,
+    price = 150000
+  },
+  ["super-warmaster's-potion"] = {
+    potions = {{"rs-super-ranging-potion", 2},
+              {"rs-super-magic-potion", 2},
+              {"rs-super-defence", 2},
+              {"rs-super-strength", 2},
+              {"rs-super-attack", 2}},
+    level = 85,
+    price = 200000
+  },
+  ["replenishment-potion"] = {
+    potions = {{"rs-adrenaline-potion", 3}, {"rs-super-restore", 3}},
+    level = 87,
+    price = 200000
+  },
+  ["wyrmfire-potion"] = {
+    potions = {{"rs-super-antifire", 3}, {"rs-antifire", 4}, {"rs-extended-super-antifire", 2}},
+    level = 89,
+    price = 200000
+  },
+  ["enhanced-replenishment-potion"] = {
+    potions = {{"rs-replenishment-potion", 3}},
+    secondaries = {{"rs-adrenaline-crystal", 1}},
+    level = 90,
+    price = 0
+  },
+  ["extreme-brawler's-potion"] = {
+    potions = {{"rs-extreme-attack", 3}, {"rs-extreme-defence", 3}, {"rs-extreme-strength", 3}, {"rs-super-melee-potion", 2}},
+    level = 91,
+    price = 300000
+  },
+  ["extreme-battlemage's-potion"] = {
+    potions = {{"rs-extreme-magic", 3}, {"rs-extreme-defence", 3}, {"rs-grand-magic-potion", 2}},
+    level = 91,
+    price = 300000
+  },
+  ["extreme-sharpshooter's-potion"] = {
+    potions = {{"rs-extreme-ranging", 3}, {"rs-extreme-defence", 3}, {"rs-grand-ranging-potion", 2}},
+    level = 92,
+    price = 300000
+  },
+  ["extreme-warmaster's-potion"] = {
+    potions = {{"rs-extreme-attack", 1},
+              {"rs-extreme-defence", 1},
+              {"rs-extreme-strength", 1},
+              {"rs-extreme-ranging", 1},
+              {"rs-extreme-magic", 1},
+              {"rs-super-warmaster's-potion", 3}},
+    level = 93,
+    price = 400000
+  },
+  ["supreme-strength-potion"] = {
+    potions = {{"rs-extreme-strength", 4}, {"rs-super-strength", 4}, {"rs-grand-strength-potion", 2}},
+    level = 93,
+    price = 500000
+  },
+  ["supreme-attack-potion"] = {
+    potions = {{"rs-extreme-attack", 4}, {"rs-super-attack", 4}, {"rs-grand-attack-potion", 2}},
+    level = 93,
+    price = 500000
+  },
+  ["supreme-defence-potion"] = {
+    potions = {{"rs-extreme-defence", 4}, {"rs-super-defence", 4}, {"rs-grand-defence-potion", 2}},
+    level = 93,
+    price = 500000
+  },
+  ["supreme-magic-potion"] = {
+    potions = {{"rs-extreme-magic", 4}, {"rs-super-magic-potion", 4}, {"rs-grand-magic-potion", 2}},
+    level = 93,
+    price = 500000
+  },
+  ["supreme-ranging-potion"] = {
+    potions = {{"rs-extreme-ranging", 4}, {"rs-super-ranging-potion", 4}, {"rs-grand-ranging-potion", 2}},
+    level = 93,
+    price = 500000
+  },
+  ["brightfire-potion"] = {
+    potions = {{"rs-prayer-renewal", 3}, {"rs-super-antifire", 3}},
+    level = 94,
+    price = 600000
+  },
+  ["super-prayer-renewal-potion"] = {
+    potions = {{"rs-prayer-renewal", 4}, {"rs-super-prayer", 3}, {"rs-prayer-potion", 5}},
+    level = 96,
+    price = 750000
+  },
+  ["holy-overload-potion"] = {
+    potions = {{"rs-overload", 2}, {"rs-prayer-renewal", 2}},
+    level = 97,
+    price = 700000
+  },
+  ["searing-overload-potion"] = {
+    potions = {{"rs-overload", 4}, {"rs-super-antifire", 2}, {"rs-extended-super-antifire", 2}},
+    level = 97,
+    price = 700000
+  },
+  ["overload-salve"] = {
+    potions = {{"rs-overload", 3},
+              {"rs-super-antifire", 2},
+              {"rs-antifire", 2},
+              {"rs-prayer-renewal", 2},
+              {"rs-prayer-potion", 2},
+              {"rs-super-antipoison", 2},
+              {"rs-searing-overload-potion", 2},
+              {"rs-super-prayer-renewal-potion", 2}},
+    level = 97,
+    price = 800000
+  },
+  ["aggroverload"] = {
+    potions = {{"rs-overload", 3}, {"rs-aggression-potion", 3}},
+    secondaries = {{"rs-clean-arbuck", 1}},
+    level = 97,
+    price = 1000000
+  },
+  ["supreme-overload-potion"] = {
+    potions = {{"rs-overload", 3},
+              {"rs-supreme-attack-potion", 2},
+              {"rs-supreme-strength-potion", 2},
+              {"rs-supreme-defence-potion", 2},
+              {"rs-supreme-ranging-potion", 2},
+              {"rs-supreme-magic-potion", 2}},
+    level = 98,
+    price = 900000
+  },
+  ["supreme-overload-salve"] = { -- Best speed module that can be used for all recipes
+    potions = {{"rs-supreme-overload-potion", 3},
+              {"rs-overload-salve", 2},
+              {"rs-super-antifire", 1},
+              {"rs-antifire", 1},
+              {"rs-prayer-renewal", 1},
+              {"rs-prayer-potion", 1},
+              {"rs-super-antipoison", 1}},
+    level = 99,
+    price = 100000
+  },
+  ["perfect-plus-potion"] = { -- TODO. I'd like this potion to be a general production module for every skill, so needs to have all skill potions in them
+    potions = {{"rs-overload", 3}},
+    secondaries = {{"rs-harmony-moss", 3}, {"rs-crystal-tree-blossom", 3}},
+    level = 99,
+    price = 1000000
+  }
+}
